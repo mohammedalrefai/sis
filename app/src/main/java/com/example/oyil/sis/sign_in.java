42514ab594f8d13ba204d162f73cbe8dd37ce7f3
abdallah <mhdrfi95@gmail.com>
2018-12-11 00:35:05 -0800

    sign in file update

--- app/src/main/java/com/example/oyil/sis/sign_in.java
+++ app/src/main/java/com/example/oyil/sis/sign_in.java
@@ -2,12 +2,17 @@ package com.example.oyil.sis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class sign_in extends AppCompatActivity {
+
+    private Button sign_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+
     }
 }

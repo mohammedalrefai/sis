c862a9d7378548283ad7e8f58d2dda938197d37b
abdallah <mhdrfi95@gmail.com>
2018-12-12 01:16:16 -0800

    sign in file update

--- app/src/main/java/com/example/oyil/sis/sign_in.java
+++ app/src/main/java/com/example/oyil/sis/sign_in.java
@@ -1,8 +1,11 @@
 package com.example.oyil.sis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class sign_in extends AppCompatActivity {
 
@@ -12,6 +15,16 @@ public class sign_in extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        sign_in = (Button) findViewById(R.id.sign_in);
+
+        sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent international_intent =new  Intent(sign_in.this,international_student_info.class);
+                startActivity(international_intent);
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/oyil/sis/sis.java
+++ app/src/main/java/com/example/oyil/sis/sis.java
@@ -1,13 +1,23 @@
 package com.example.oyil.sis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class sis extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sis);
+
+
+
+
     }
 }

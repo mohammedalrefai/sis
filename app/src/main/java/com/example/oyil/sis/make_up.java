ae95bbfa29beff90197f63979319c88eeafaa970
abdallah <mhdrfi95@gmail.com>
2018-12-17 05:02:25 -0800

    sign in file update

---
+++ app/src/main/java/com/example/oyil/sis/make_up.java
@@ -0,0 +1,13 @@
+package com.example.oyil.sis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class make_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_up);
+    }
+}
---
+++ app/src/main/java/com/example/oyil/sis/stud_tab.java
@@ -0,0 +1,13 @@
+package com.example.oyil.sis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class stud_tab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stud_tab);
+    }
+}

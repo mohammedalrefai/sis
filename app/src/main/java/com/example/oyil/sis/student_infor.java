0ecdbd7f76d6de1d01a17ef4337f5cd945946817
abdallah <mhdrfi95@gmail.com>
2019-01-24 11:33:38 +0200

    sign in file update

--- app/src/main/java/com/example/oyil/sis/student_infor.java
+++ app/src/main/java/com/example/oyil/sis/student_infor.java
@@ -11,7 +11,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import android.widget.Button;
-
+import android.widget.TextView;
 
 
 public class student_infor extends AppCompatActivity implements View.OnClickListener {
@@ -19,6 +19,28 @@ public class student_infor extends AppCompatActivity implements View.OnClickList
     private Button scholarship;
 
     private Button back;
+
+    private Button log_out;
+
+    private TextView st_name;
+
+    private TextView collage;
+
+    private TextView department;
+
+    private TextView study_filed;
+
+    private TextView academic_status;
+
+    private TextView gender;
+
+    private TextView expected_graduate;
+
+    private TextView regstration_date;
+
+    private TextView regstration_date_1;
+
+    private TextView regstration_date_2;
 
     @Override
 
@@ -42,7 +64,30 @@ public class student_infor extends AppCompatActivity implements View.OnClickList
 
         back = (Button) findViewById(R.id.back_bt);
 
+        log_out = (Button) findViewById(R.id.log_out_bt);
 
+        st_name= (TextView) findViewById(R.id.st_name);
+
+        collage = (TextView) findViewById(R.id.collage);
+
+        department = (TextView) findViewById(R.id.department);
+
+        study_filed = (TextView) findViewById(R.id.study_filed);
+
+        academic_status = (TextView) findViewById(R.id.academic_status);
+
+        gender = (TextView) findViewById(R.id.gender);
+
+        expected_graduate= (TextView) findViewById(R.id.expected_graduate);
+
+        regstration_date = (TextView) findViewById(R.id.regstration_date);
+
+        regstration_date_1 = (TextView) findViewById(R.id.regstration_date_1);
+
+        regstration_date_2 = (TextView) findViewById(R.id.regstration_date_2);
+
+
+        log_out.setOnClickListener(this);
 
         scholarship.setOnClickListener(this);
 
